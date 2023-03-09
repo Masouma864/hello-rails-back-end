5a2ca06f235de6c3679beda0795f97bee85bc457
masouma <masouma2020@gmail.com>
2023-03-09 16:55:15 +0430

    Update the rack cors to whitelist frontend

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module HelloRailsBackEnd
 end
 config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000' # replace this with your React server's URL
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    origins 'localhost:3001'# replace this with your React server's URL
+    resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
