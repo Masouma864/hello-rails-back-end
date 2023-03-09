7170ac1922a592fef5b3d19795ba3f3dbadbc8e9
masouma <masouma2020@gmail.com>
2023-03-09 16:35:53 +0430

    Add Api and version module to the api controller

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::GreetingsController < ApplicationController
+    before_action :set_greeting, only: %i[ show edit update destroy ]
+  
+    # GET /greetings or /greetings.json
+    def index
+      @greetings = Greeting.all.slice(:content)
+    end
+  
+  end
+  
